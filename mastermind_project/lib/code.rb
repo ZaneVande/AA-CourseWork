b692f40cd382100330e32d9c48f8c31dbeb2f674
Zane Vandergriff <zanevande@gmail.com>
2021-05-06 19:14:24 -0400

    Finsihed Mastermind Project

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -6,11 +6,57 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
   def self.valid_pegs?(chars)
     chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase)}
   end
 
   def initialize(chars)
-    Code.valid_pegs?(chars)
+    if Code.valid_pegs?(chars)
+      @pegs = chars.map(&:upcase)
+    else
+      raise "contains non valid pegs"
+    end
+  end
+
+  def self.random(number)
+    randoms = []
+    number.times { randoms << POSSIBLE_PEGS.keys.sample }
+    Code.new(randoms)
+  end
+
+  def self.from_string(string)
+    Code.new(string.split(""))
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+   @pegs.length
+  end
+
+  def num_exact_matches(guess)
+    count = 0
+    (0...guess.length).each do |idx|
+      count += 1 if guess[idx] == self[idx]
+    end
+      count
+  end
+
+  def num_near_matches(guess)
+    count = 0
+    (0...guess.length).each do |idx|
+      if guess[idx] != self[idx] && self.pegs.include?(guess[idx])
+        count += 1
+      end
+    end
+    count
+  end
+
+  def ==(another)
+    self.pegs == another.pegs
   end
 end
--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+    
+    def initialize(number)
+        @secret_code = Code.random(number)
+    end
 
+    def print_matches(code)
+      puts "exact matches: #{@secret_code.num_exact_matches(code)}"
+      puts "near matches: #{@secret_code.num_near_matches(code)}"
+    end
+
+    def ask_user_for_guess
+        puts "Enter a code: "
+        guess = Code.from_string(gets.chomp)
+        self.print_matches(guess)
+        @secret_code == guess
+    end
 end
