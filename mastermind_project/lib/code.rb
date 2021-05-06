3410b9cfbfb709dccc6cd9f3186fbcd76d6ee29b
Zane Vandergriff <zanevande@gmail.com>
2021-05-06 13:41:28 -0400

    Adding Mastermind and Battleship Projects

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -6,4 +6,11 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase)}
+  end
+
+  def initialize(chars)
+    Code.valid_pegs?(chars)
+  end
 end
