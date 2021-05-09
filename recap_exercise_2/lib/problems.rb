62072b98f8a2aea4ec32c4f75dbfef7863f4f0ca
Zane Vandergriff <zanevande@gmail.com>
2021-05-09 18:55:24 -0400

    Adding 3 Foundations Recap Exercises

---
+++ recap_exercise_2/lib/problems.rb
@@ -0,0 +1,45 @@
+# Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
+# of both of the given numbers
+def least_common_multiple(num_1, num_2)
+
+end
+
+
+# Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
+# most in the string.
+def most_frequent_bigram(str)
+
+end
+
+
+class Hash
+    # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
+    def inverse
+
+    end
+end
+
+
+class Array
+    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
+    def pair_sum_count(num)
+
+    end
+
+    # Write a method, Array#bubble_sort, that takes in an optional proc argument.
+    # When given a proc, the method should sort the array according to the proc.
+    # When no proc is given, the method should sort the array in increasing order.
+    #
+    # Sorting algorithms like bubble_sort, commonly accept a block. That block accepts
+    # two parameters, which represents the two elements in the array being compared.
+    # If the block returns 1, it means that the second element passed to the block
+    # should go before the first (i.e. switch the elements). If the block returns -1,
+    # it means the first element passed to the block should go before the second
+    # (i.e. do not switch them). If the block returns 0 it implies that
+    # it does not matter which element goes first (i.e. do nothing).
+    #
+    # This should remind you of the spaceship operator! Convenient :)
+    def bubble_sort(&prc)
+
+    end
+end
